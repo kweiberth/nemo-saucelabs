408cecc416172f7a1610960a29dc0268863dc5a4
kweiberth <kweiberth@paypalcorp.com>
2017-05-25 12:27:06 -0700

    upgrade for nemo@3.0.0 and selenium@3.0.0
    **What** and **Why**:
    
    * `selenium@3.0.0` was recently released, which starts a process of deprecating selenium's built-in promise manager. More info [here](https://github.com/SeleniumHQ/selenium/issues/2969)
    * Is also seems that some of the `driver` and `session` APIs have changed
    	* e.g. you must now use `session.getId()` instead of `session.id_` (which was being used in this plugin)
    * This PR removes dependency on selenium's promise manager
    * It also fixes `getJobUrl` method, which is broken due to breaking changes in `selenium@3.0.0`
    * Updated README to reflect these changes
    
    **Other changes**:
    
    * due to the fact that `selenium@3.0.0` requires `node@6.0.0` or later, I updated some of the syntax
    	* e.g. change `var` to `let` or `const`
    * Removed `Gruntfile.js` since there were no tests written at all in this repo, then added a "todo" `echo` statement for `npm test`
    * Switched `_.extend` for native `Object.assign`
    	* also, since we then we're using `underscore` only for `_.defaults`, pulled in `lodash.defaults` instead of full `underscore` library
    
    **Questions**:
    
    * should this be made into a separate `3.x` branch?
    
    **Reviewers**:

--- index.js
+++ index.js
@@ -22,22 +22,15 @@
  |  THE SOFTWARE.                                                                            |
  \*---------------------------------------------------------------------------------------- */
 
-'use strict';
-var SauceLabs = require("./lib/saucelabs");
+const SauceLabs = require('./lib/saucelabs');
 
-module.exports = {
-
-    setup: function(nemo, callback) {
-
-        var options = {
-            username: nemo._config.get('driver').serverCaps.username,
-            accessKey: nemo._config.get('driver').serverCaps.accessKey,
-            driver: nemo.driver,
-            wd: nemo.wd
-        };
-
-        nemo.saucelabs = new SauceLabs(options);
-
-        callback(null);
-    }
-};
+module.exports.setup = function setup(nemo, callback) {
+  const options = {
+      username: nemo._config.get('driver').serverCaps.username,
+      accessKey: nemo._config.get('driver').serverCaps.accessKey,
+      driver: nemo.driver,
+      wd: nemo.wd
+  };
+  nemo.saucelabs = new SauceLabs(options);
+  callback(null);
+}
